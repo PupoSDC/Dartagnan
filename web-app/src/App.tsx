46baed59bf0f466fce138f394603435f920cf147
PupoSDC <pedro.pupo@greator.com>
2021-02-26 18:08:35 +0100

    Add major dependencies

---
+++ web-app/src/App.tsx
@@ -0,0 +1,10 @@
+import { FunctionComponent } from 'react';
+import Button from '@material-ui/core/Button';
+
+const App : FunctionComponent = () => (
+  <Button variant="contained" color="primary">
+    Hello World
+  </Button>
+);
+
+export default App;
